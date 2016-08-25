762c63e8942a73cb06e4204ba897ba13d7b59f95
Andres Moschini <andresmoschini@gmail.com>
2016-08-25 13:22:25 -0300

    Add SMTP example

--- php-smtp-cli-example/php-smtp-cli-example.php
+++ php-smtp-cli-example/php-smtp-cli-example.php
@@ -1,2 +1,40 @@
 <?php
-echo "Hi PHP!";
+
+// SwiftMailer previously installed
+// See https://github.com/DopplerRelay/docker-php/blob/master/Dockerfile
+include_once "/usr/lib/vendor/swiftmailer/swiftmailer/lib/swift_required.php";
+
+// Get username and password from environment variables
+$username = getenv('DOPPLERRELAY_USERNAME');
+$password = getenv('DOPPLERRELAY_PASSWORD');
+
+// Relay SMTP service configuration
+$host = 'smtp.dopplerrelay.com';
+$port = 587;
+
+// Custom data
+$from = array('you@yourdomain.com' => 'Your Name');
+$to = array(
+    'recipient1@example.com'  => 'Recipient1 Name',
+    'recipient2@example2.com' => 'Recipient2 Name'
+);
+$subject = 'Hello from Doppler Relay, PHP!';
+$text = "Doppler Relay speaks plaintext";
+$html = "Doppler Relay speaks <b>HTML</b>";
+
+// Send message using SwiftMailer
+$transport = Swift_SmtpTransport::newInstance($host, $port);
+$transport->setUsername($username);
+$transport->setPassword($password);
+$swift = Swift_Mailer::newInstance($transport);
+$message = new Swift_Message($subject);
+$message->setFrom($from);
+$message->setBody($html, 'text/html');
+$message->setTo($to);
+$message->addPart($text, 'text/plain');
+if ($recipients = $swift->send($message, $failures)) {
+    echo 'Message successfully sent!';
+} else {
+    echo "There was an error:\n";
+    print_r($failures);
+}
